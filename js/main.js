6c8781a2abf05a2830336d23afd871e66fd27002
svetaelar <svetaelar@gmail.com>
2020-01-18 14:57:31 +0300

    Пытаюсь подружиться с Трэвисом

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
+'use strict';
+
 var fireballSize = 22;
 var wizardSpeed = 3;
 var wizardWidth = 70;
 var getFireballSpeed = function (left) {
   left ? wizardSpeed = 5 : wizardSpeed = 3
   return wizardSpeed;
-}
+};
 
 var getWizardHeight = function () {
   return 1.337 * wizardWidth
-}
+};
 
 getWizardX = function (width) {
-  return (width - wizardWidth)/2;
-}
+  return (width - wizardWidth) / 2;
+};
 
-console.log (getWizardHeight);
 getWizardY = function (height) {
-  return height/3 + getWizardHeight()/2;
-}
+  return height / 3 + getWizardHeight() / 2;
+};
