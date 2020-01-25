c975ee4b898ac5f520f8af46686b71eba6cdaf5e
svetaelar <svetaelar@gmail.com>
2020-01-26 00:50:00 +0300

    Добавила объект

--- js/setup.js
+++ js/setup.js
@@ -1,17 +1,11 @@
 'use strict';
 
-var NAMES = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
-var wizards = [
-  {
-    name: NAMES[0],
-    surname: SURNAMES[0],
-    coatColor: COAT_COLOR[0],
-    eyesColor: EYES_COLOR[0]
-  }
-];
+var wizards = {
+  NAMES: ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
+  SURNAMES: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
+  COAT_COLOR: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
+  EYES_COLOR: ['black', 'red', 'blue', 'yellow', 'green']
+};
 
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
@@ -28,11 +22,11 @@ var randomElement = function (arr) {
   return arr[i];
 };
 
-var renderWizard = function (wizard) {
+var renderWizard = function (arr) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
-  wizardElement.querySelector('.setup-similar-label').textContent = randomElement(NAMES) + ' ' + randomElement(SURNAMES);
-  wizardElement.querySelector('.wizard-coat').style.fill = randomElement(COAT_COLOR);
-  wizardElement.querySelector('.wizard-eyes').style.fill = randomElement(EYES_COLOR);
+  wizardElement.querySelector('.setup-similar-label').textContent = randomElement(arr.NAMES) + ' ' + randomElement(arr.SURNAMES);
+  wizardElement.querySelector('.wizard-coat').style.fill = randomElement(arr.COAT_COLOR);
+  wizardElement.querySelector('.wizard-eyes').style.fill = randomElement(arr.EYES_COLOR);
   return wizardElement;
 };
 
