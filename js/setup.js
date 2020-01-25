0a77da4c6d0ee90a6f2296e4eaa4e7295f638076
svetaelar <svetaelar@gmail.com>
2020-01-26 00:56:15 +0300

    Убирает повтор в коде

--- js/setup.js
+++ js/setup.js
@@ -10,8 +10,6 @@ var wizards = {
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
-document.querySelector('.setup-similar').classList.remove('hidden');
-
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
